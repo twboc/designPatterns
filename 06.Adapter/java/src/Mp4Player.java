a698290b6100c55548539a76a5ae9db8c76e8e33
twboc <hubert.sobkow@br-ag.eu>
2017-03-05 15:10:15 +0100

    06.Adapter pattern implemented in java

---
+++ 06.Adapter/java/src/Mp4Player.java
@@ -0,0 +1,10 @@
+
+public class Mp4Player implements AdvancedMediaPlayer {
+
+	public void playVlc(String fileName){	}
+
+	public void playMp4(String fileName){
+		System.out.println("Now playing a mp4 file: " + fileName);
+	}
+
+}
---
+++ 06.Adapter/java/src/VlcPlayer.java
@@ -0,0 +1,14 @@
+
+public class VlcPlayer implements AdvancedMediaPlayer {
+	
+//	@Override
+	public void playVlc(String fileName){
+		
+		System.out.println("Now playing a vlc file: " + fileName);
+		
+	}
+	
+//	@Override
+	public void playMp4(String fileName){	}
+
+}
